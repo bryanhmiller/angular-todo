04ef4b39bcdf31c3e97b97ae18b26480d890b424
bryanhmiller <bryankalos@hotmail.com>
2017-05-15 21:33:16 -0500

    Added factory and wired it

--- javascripts/controllers/ItemListCtrl.js
+++ javascripts/controllers/ItemListCtrl.js
@@ -1,34 +1,17 @@
-app.controller("ItemListCtrl", function($http, $q, $scope, FIREBASE_CONFIG) {
+app.controller("ItemListCtrl", function($scope, ItemFactory) {
   console.log("inside ItemListCtrl");
 
 	$scope.items = [];
 
-  let getItemList = () => {
-    let itemz = [];
-    return $q((resolve, reject) => {
-      $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
-      .then((fbItems) => {
-          var itemCollection = fbItems.data;
-          Object.keys(itemCollection).forEach((key) => {
-            itemCollection[key].id=key;
-            itemz.push(itemCollection[key]);
-          });
-          resolve(itemz);
-      }).catch((error) => {
-        reject(error);
-      });  
-    });  
-  };
 
-  let getItems = () => {
-    getItemList().then((itemz) => {
+  ItemFactory.getItemList().then((itemz) => {
       $scope.items = itemz;
       console.log("itemz", itemz);
     }).catch((error) => {
       console.log("get Error", error);
     });
-  };
+  
 
-  getItems();
+  ItemFactory.getItemList();
 
 });
--- javascripts/controllers/ItemNewCtrl.js
+++ javascripts/controllers/ItemNewCtrl.js
@@ -1,22 +1,11 @@
 app.controller("ItemNewCtrl", function($http, $q, $scope, FIREBASE_CONFIG) {
   console.log("inside ItemNewCtrl");
 
-  let postNewItem = (newItem) => {
-    return $q ((resolve, reject) => {
-      $http.post(`${FIREBASE_CONFIG.databaseURL}/items.json`, JSON.stringify(newItem))
-      .then((resultz) => {
-        resolve(resultz);
-      }).catch((error) => {
-        reject(error);
-      });
-    });
-  };
-
   $scope.addNewItem = () => {
     $scope.newTask.isCompleted = false;
     postNewItem($scope.newTask).then((response) => {
       $scope.newTask = {};
-      getItems();
+      //switch views
     }).catch((Error) => {
       console.log("Add error", error);
     });
