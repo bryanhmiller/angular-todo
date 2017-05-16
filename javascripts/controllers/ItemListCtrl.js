0f0c058f48ed2fba6b14b4a72cdad73b57ddedb3
bryanhmiller <bryankalos@hotmail.com>
2017-05-15 21:13:56 -0500

    Split files into partials and controllers

---
+++ javascripts/controllers/ItemListCtrl.js
@@ -0,0 +1,34 @@
+app.controller("ItemListCtrl", function($http, $q, $scope, FIREBASE_CONFIG) {
+  console.log("inside ItemListCtrl");
+
+	$scope.items = [];
+
+  let getItemList = () => {
+    let itemz = [];
+    return $q((resolve, reject) => {
+      $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
+      .then((fbItems) => {
+          var itemCollection = fbItems.data;
+          Object.keys(itemCollection).forEach((key) => {
+            itemCollection[key].id=key;
+            itemz.push(itemCollection[key]);
+          });
+          resolve(itemz);
+      }).catch((error) => {
+        reject(error);
+      });  
+    });  
+  };
+
+  let getItems = () => {
+    getItemList().then((itemz) => {
+      $scope.items = itemz;
+      console.log("itemz", itemz);
+    }).catch((error) => {
+      console.log("get Error", error);
+    });
+  };
+
+  getItems();
+
+});
